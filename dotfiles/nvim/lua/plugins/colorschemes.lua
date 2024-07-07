dd912d4603112c1fc32d7b4e5b6f18e23ac89230
ryangeor <ryangeor@mainbox>
2024-07-07 03:30:52 +0100

    Fluoro

--- dotfiles/nvim/lua/plugins/colorschemes.lua
+++ dotfiles/nvim/lua/plugins/colorschemes.lua
@@ -1,12 +1,33 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
-    priority=1000,
     config=function() 
 	    vim.o.background = "dark"
 	    require("gruvbox").setup()
-	    vim.cmd("colorscheme gruvbox")
+	    vim.keymap.set('n', '<leader>bg', ':colorscheme gruvbox<CR>')
     end,
     },
+    {
+    "samharju/synthweave.nvim",
+    config=function() 
+	    vim.keymap.set('n', '<leader>bs', ':colorscheme synthweave<CR>')
+    end
+    },
+    {
+    "maxmx03/fluoromachine.nvim",
+    priority=1000,
+    config=function() 
+	    local fm = require 'fluoromachine'
+	    fm.setup {
+		    flow = true,
+		    --theme='delta',
+		     theme='fluoromachine',
+		    -- theme='retrowave',
+	    }
+	    vim.cmd("colorscheme fluoromachine")
+	    vim.keymap.set('n', '<leader>bf', ':colorscheme fluoromachine<CR>')
+    end
+    },
+
 }
 
