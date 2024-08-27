afd141187782b5408bb7467306c72733204589a2
gajr <g.aj.ryan@live.co.uk>
2024-08-27 11:01:59 +0100

    luasnip + snippets for C

--- dotfiles/nvim/lua/plugins/snippets.lua
+++ dotfiles/nvim/lua/plugins/snippets.lua
@@ -5,59 +5,86 @@ return {
 		version="v2.*",
 		config=function ()
 			local ls=require "luasnip"
-			local snippet_path = "lua/snippets"
+			local snippet_path = "/lua/snippets/"
 			local snippet_files = vim.api.nvim_get_runtime_file(snippet_path .. "*.lua", true)
+			print("Found snippet files:", vim.inspect(snippet_files))
 			for _, fpath in ipairs(snippet_files) do
 				local filename = vim.fn.fnamemodify(fpath, ":t:r")
+				print("Loading snippets for filetype:", filename)
 				local chunk = loadfile(fpath)
 				if chunk then
 					local snippets = chunk()
 					if type(snippets) == "table" then
+						print("Loaded", #snippets, "snippets for", filename)
 						ls.add_snippets(filename, snippets)
+					else
+						print("Error: Snippet file", fpath, "did not return a table")
 					end
+				else
+					print("Error loading file:", fpath)
 				end
 				loadfile(fpath)()
 			end
 
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.snippet.active=function(filter)
-				filter = filter or {}
-				if filter.direction == 1 then
-					return ls.expand_or_jumpable
-				else
-					return ls.jumpable(filter.direction)
+		--	---@diagnostic disable-next-line: duplicate-set-field
+		--	vim.snippet.active=function(filter)
+		--		filter = filter or {}
+		--		if filter.direction == 1 then
+		--			return ls.expand_or_jumpable
+		--		else
+		--			return ls.jumpable(filter.direction)
+		--		end
+		--	end
+
+		--	---@diagnostic disable-next-line: duplicate-set-field
+		--	vim.snippet.jump = function(direction)
+		--		if direction == 1 then
+		--			if ls.expandable() then
+		--				return ls.expand_or_jump()
+		--			else
+		--				return ls.jumpable(1) and ls.jump(1)
+		--			end
+		--		else
+		--			return ls.jumpable(-1) and ls.jump(-1)
+		--		end
+		--	end
+
+		--	vim.snippet.stop = ls.unlink_current
+
+			--ls.config.set_config {
+			--	history=true,
+			--	updateevents="TextChanged,TextChangedI",
+			--	override_builtin=true,
+			--}
+			--
+			vim.keymap.set({"i", "s"}, "<C-i>", function()
+				if ls.expand_or_jumpable() then
+					ls.expand_or_jump()
 				end
-			end
+			end, {silent = true})
 
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.snippet.jump = function(direction)
-				if direction == 1 then
-					if ls.expandable() then
-						return ls.expand_or_jump()
-					else
-						return ls.jumpable(1) and ls.jump(1)
-					end
-				else
-					return ls.jumpable(-1) and ls.jump(-1)
+			vim.keymap.set({"i", "s"}, "<C-k>", function()
+				if ls.jumpable(-1) then
+					ls.jump(-1)
 				end
-			end
+			end, {silent = true})
 
-			vim.snippet.stop = ls.unlink_current
-
-			ls.config.set_config {
-				history=true,
-				updateevents="TextChanged,TextChangedI",
-				override_builtin=true,
-			}
-
-
-			vim.keymap.set({"i", "s"}, "<leader><C-K>", function()
-				return vim.snippet.active{direction=1} and vim.snippet.jump(1)
-			end, {silent=true})
-
-			vim.keymap.set({"i", "s"}, "<leader><C-L>", function()
-				return vim.snippet.active{direction=1} and vim.snippet.jump(1)
-			end, {silent=true})
+			vim.keymap.set("i", "<C-l>", function()
+				if ls.choice_active() then
+					ls.change_choice(1)
+				end
+			end)
+			vim.api.nvim_create_user_command("ExpandSnippet", function(opts)
+				local snip_name = opts.args
+				--print("Trying to expand snippet:", snip_name)
+				local snippets = ls.get_snippets("c")
+				if snippets and snippets[snip_name] then
+					ls.snip_expand(snippets[snip_name])
+				else
+					print("snippet not found:", snip_name)
+					print("available snippets:", vim.inspect(snippets))
+				end
+			end, {nargs = 1})
 
 		end
 
--- dotfiles/nvim/lua/snippets/c.lua
+++ dotfiles/nvim/lua/snippets/c.lua
@@ -1,48 +1,45 @@
 local ls = require("luasnip")
-
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
 
-return {
-	-- Main function with argument parsing
+local snippets = {
 	s("cmain", {
-		t({"#include <stdio.h>",
-		"#include <stdlib.h>",
-		"#include <string.h>",
-		"#include <getopt.h>",
-		"",
-		"int main(int argc, char *argv[]) {",
-		"    int opt;",
-		"    while ((opt = getopt(argc, argv, \""}),
-		i(1, "hi:o:"),
-		t({"\")) != -1) {",
-		"        switch (opt) {",
-		"            case 'h':",
-		"                printf(\"Usage: %s ", "[-h] "}),
-		i(2, "[-i input_file] [-o output_file]"),
-		t({"\", argv[0]);",
-		"                return 0;",
-		"            case 'i':",
-		"                // sme input file",
-		"                break;",
-		"            case 'o':",
-		"                // sme output file",
-		"                break;",
-		"            default:",
-		"                fprintf(stderr, \"Usage: %s ", "[-h] "}),
-		f(function(args) return args[1][1] end, {2}),
-		t({"\\n\", argv[0]);",
-		"                return 1;",
-		"        }",
-		"    }",
-		"",
-		"    // Your code here",
-		"",
-		"    return 0;",
-		"}"}),
+		t({
+			"#include <stdio.h>",
+			"#include <stdlib.h>",
+			"#include <unistd.h>",
+			"",
+			"int main(int argc, char *argv[]) {",
+			"\tint opt;",
+			"\tchar *ifile = NULL;",
+			"\tchar *ofile = NULL;",
+			"\twhile ((opt = getopt(argc, argv, \"hi:o:\")) != -1) {",
+			"\t\tswitch (opt) {",
+			"\t\tcase 'h':",
+			"\t\t\tprintf(\"Usage: %s [-h] [-i input_file] [-o output_file]\\n\", argv[0]);",
+			"\t\t\treturn 0;",
+			"\t\tcase 'i':",
+			"\t\t\tifile = optarg;",
+			"\t\t\tbreak;",
+			"\t\tcase 'o':",
+			"\t\t\tofile = optarg;",
+			"\t\t\tbreak;",
+			"\t\tdefault:",
+			"\t\t\tfprintf(stderr, \"Usage: %s [-h] [-i input_file] [-o output_file]\\n\", argv[0]);",
+			"\t\t\treturn 1;",
+			"\t\t}",
+			"\t}",
+			"",
+			"\t// Your code here",
+			"\tif (ifile) printf(\"Input file: %s\\n\", ifile);",
+			"\tif (ofile) printf(\"Output file: %s\\n\", ofile);",
+			"",
+			"\treturn 0;",
+			"}"
+		})
 	}),
 
 	-- read a file into mem
@@ -51,8 +48,8 @@ return {
 		i(1, "filename"),
 		t({", \"r\");",
 		"if (fp == NULL) {",
-		"    perror(\"Error opening file\");",
-		"    return 1;",
+		"perror(\"Error opening file\");",
+		"\treturn 1;",
 		"}",
 		"",
 		"char *line = NULL;",
@@ -60,33 +57,35 @@ return {
 		"ssize_t read;",
 		"",
 		"while ((read = getline(&line, &len, fp)) != -1) {",
-		"    // process line",
-		"    "}),
+		"\t// process line",
+		"\t"}),
 		i(2, "printf(\"%s\", line);"),
 		t({"",
 		"}",
 		"",
 		"fclose(fp);",
 		"if (line)",
-		"    free(line);"}),
+		"\tfree(line);"}),
 	}),
+
 
 	-- err maco
 	s("cerr", {
 		t({"#define ERROR_CHECK(condition, message) \\",
-		"    do { \\",
-		"        if (condition) { \\",
-		"            fprintf(stderr, \"Error: %s\\n\", message); \\",
-		"            "}),
+		"\tdo { \\",
+		"\t\tif (condition) { \\",
+		"\t\t\tfprintf(stderr, \"Error: %s\\n\", message); \\",
+		"\t\t\t"}),
 		c(1, {
 			t("exit(1);"),
 			t("return 1;"),
 			t("goto cleanup;"),
 		}),
 		t({" \\",
-		"        } \\",
-		"    } while (0)"}),
+		"\t\t} \\",
+		"\t} while (0)"}),
 	}),
-	-- Add more C snippets here...
 }
+print("C snippets table created with", #snippets, "snippets")
+return snippets
 
